329c56e87858cb19ccc1c500be563811e7bdff71
tayebel <t.elmehadji@gmail.com>
2023-06-24 02:47:01 +0200

    js

--- script.js
+++ script.js
@@ -1,17 +1,41 @@
+// Copy code function
 function copyCode() {
   const codeElement = document.querySelector('.code code');
-  const codeText = codeElement.innerText.trim();
+  const code = codeElement.innerText;
 
-  const tempInput = document.createElement('textarea');
-  tempInput.value = codeText;
-  document.body.appendChild(tempInput);
-  tempInput.select();
-  document.execCommand('copy');
-  document.body.removeChild(tempInput);
-
-  const copyNotification = document.getElementById('copyNotification');
-  copyNotification.style.display = 'inline-block';
-  setTimeout(function() {
-    copyNotification.style.display = 'none';
-  }, 2000);
+  navigator.clipboard.writeText(code)
+    .then(() => {
+      showCopyNotification();
+    })
+    .catch((error) => {
+      console.error('Failed to copy code: ', error);
+    });
 }
+
+// Show copy notification
+function showCopyNotification() {
+  const copyNotification = document.getElementById('copyNotification');
+  copyNotification.style.display = 'block';
+  setTimeout(() => {
+    copyNotification.style.display = 'none';
+  }, 1500);
+}
+
+// Initialize clipboard.js
+const clipboard = new ClipboardJS('.copy-button', {
+  text: function (trigger) {
+    const codeElement = trigger.closest('.code-container').querySelector('code');
+    return codeElement.innerText;
+  }
+});
+
+// Handle copy success
+clipboard.on('success', function (e) {
+  showCopyNotification();
+  e.clearSelection();
+});
+
+// Handle copy error
+clipboard.on('error', function (e) {
+  console.error('Failed to copy code: ', e.action);
+});
