88dda0b09c8bc4e4b0a523c572d4181e90c3c69b
tayebel <t.elmehadji@gmail.com>
2023-06-24 02:10:17 +0200

    s

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 function copyCode() {
   const codeElement = document.querySelector('.code code');
-  const codeText = codeElement.innerText;
+  const codeText = codeElement.innerText.trim();
 
-  navigator.clipboard.writeText(codeText).then(function() {
-    const copyNotification = document.getElementById('copyNotification');
-    copyNotification.style.display = 'inline-block';
-    setTimeout(function() {
-      copyNotification.style.display = 'none';
-    }, 2000);
-  }).catch(function(error) {
-    console.error('Failed to copy code: ', error);
-  });
+  const tempInput = document.createElement('textarea');
+  tempInput.value = codeText;
+  document.body.appendChild(tempInput);
+  tempInput.select();
+  document.execCommand('copy');
+  document.body.removeChild(tempInput);
+
+  const copyNotification = document.getElementById('copyNotification');
+  copyNotification.style.display = 'inline-block';
+  setTimeout(function() {
+    copyNotification.style.display = 'none';
+  }, 2000);
 }
